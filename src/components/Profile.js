6aefb857d18caefa7c9d08bb6ee9413b12968c5a
Jahonga Sheldon <sheldoncurtis2020@gmail.com>
2025-07-10 09:30:12 +0300

    Fix all remaining missing imports

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { 
   User, Mail, Globe, Calendar, Trophy, Gamepad2, 
